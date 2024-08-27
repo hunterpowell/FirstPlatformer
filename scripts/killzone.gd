007ff3fed356b60eaef82a5bb959083a494388a7
hunterpowell <161683842+hunterpowell@users.noreply.github.com>
2024-08-26 21:59:46 -0700

    fixed death animation
    you can no longer move around after death

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,6 +5,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
+	body.alive = false
 	#sets game to half speed on death
 	Engine.time_scale = 0.5
 	timer.start()
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,8 +3,10 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+var alive = true
 var has_berry = false
 var can_double_jump = true
+var roll = false
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -14,31 +16,34 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if has_berry == true:
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-		if Input.is_action_just_pressed("jump") and not is_on_floor() and can_double_jump == true:
-			velocity.y = JUMP_VELOCITY
-			can_double_jump = false
-		if is_on_floor():
-			can_double_jump = true
-		
-	else:
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
+	if alive == true:
+		if has_berry == true:
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				velocity.y = JUMP_VELOCITY
+			if Input.is_action_just_pressed("jump") and not is_on_floor() and can_double_jump == true:
+				roll = true
+				velocity.y = JUMP_VELOCITY
+				can_double_jump = false
+			if is_on_floor():
+				can_double_jump = true
+				roll = false
+			
+		else:
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				velocity.y = JUMP_VELOCITY
 
 	# Get the input direction -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
 	
 	# Flip the sprite
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
+	if alive == true:
+		if direction > 0:
+			animated_sprite.flip_h = false
+		elif direction < 0:
+			animated_sprite.flip_h = true
 				
 	# Play animations
-	# plays death animation if engine speed is halved, only happens when killzone is hit
-	if Engine.time_scale == 0.5:
+	if alive == false:
 		animated_sprite.play("dead")
 	
 	elif is_on_floor():
@@ -48,10 +53,13 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("run")
 	
 	else:
-		animated_sprite.play("jump")
+		if roll == false:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("roll")
 	
-	# Apply movement
-	if direction:
+	# Apply movement if alive
+	if direction and alive == true:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
