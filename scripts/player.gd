9ede7ce73e28086a8330f89cabe712d6e7cd52c1
hunterpowell <161683842+hunterpowell@users.noreply.github.com>
2024-09-01 00:02:16 -0700

    Added dash

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,10 +4,15 @@ extends CharacterBody2D
 const SPEED: float = 130.0
 const JUMP_VELOCITY: float = -300.0
 const DOUBLE_JUMP_VELOCITY: float = -400.0
+const DASH_SPEED: float = 500
+
+
 var alive: bool = true
 var berries: int = 0
 var can_double_jump: bool = true
 var roll: bool = false
+var dashing: bool = false
+var can_dash: bool = true
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hit_sound: AudioStreamPlayer2D = $HitSound
@@ -18,10 +23,20 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")	# Add the gravity.
 	
+	if Input.is_action_just_pressed("dash") and can_dash:
+		dashing = true
+		can_dash = false
+		$dash_timer.start()
+		$dash_again_timer.start()
+		
+	
 	# apply gravity
-	if not is_on_floor():
+	if not is_on_floor() and not dashing:
 		velocity += get_gravity() * delta
-
+	if not is_on_floor() and dashing:
+		velocity.y = 0
+		
+		
 	handle_jump()
 	flip_sprite(direction)
 	play_animations(direction)
@@ -84,8 +99,19 @@ func play_animations(dir):
 			
 func apply_movement(dir):
 	if dir and alive == true:
-		velocity.x = dir * SPEED
+		if dashing == false:
+			velocity.x = dir * SPEED
+		else:
+			velocity.x = dir * DASH_SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_dash_timer_timeout() -> void:
+	dashing = false
+
+
+func _on_dash_again_timer_timeout() -> void:
+	can_dash = true
