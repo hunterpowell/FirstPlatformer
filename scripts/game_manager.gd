584abf691b2065986cf69c03f227caf4c0f248a8
Hunter <hunterpowell99@gmail.com>
2024-09-01 13:16:09 -0700

    Added score HUD

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,10 +3,12 @@ extends Node
 var score = 0
 
 @onready var score_label: Label = $ScoreLabel
+@onready var hud: CanvasLayer = $HUD
+@onready var hud_label: Label = $HUD/hud_label
 
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins!"
-	if score == 1:
-		score_label.text = "You collected 1 coin??? that's it?!?!? you are a massive fucking nerd"
+	
+	hud_label.text = "Coins: " + str(score)
 	
